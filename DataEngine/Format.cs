8357f243eaf24ca33c7dddce6271db1eaa305a89
dmitriano <winappdev@gmail.com>
2016-10-28 01:23:05 +0300

    removed unused format methods

--- DataEngine/Format.cs
+++ DataEngine/Format.cs
@@ -8,67 +8,6 @@ namespace DataEngine
 {
     static class Format
     {
-        //this would allow things like 999.999.999 which is not a valid (the format is valid, 
-        //but not the values) IP address
-        public static readonly Regex IpPortRegex = new Regex(@"^(?<Ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\:(?<Port>\d+)$");
-
-        public static int Ip2Num(System.Net.IPAddress ip)
-        {
-            return System.BitConverter.ToInt32(ip.GetAddressBytes(), 0);
-        }
-
-        public static string Ip2String(int ip)
-        {
-            return new IPAddress(BitConverter.GetBytes(ip)).ToString();
-        }
-
-        public static string Port2String(short port)
-        {
-            return ((ushort)port).ToString();
-        }
-
-        public static string EndPoint2String(int ip, short port)
-        {
-            return String.Format("{0}:{1}", Format.Ip2String(ip), Format.Port2String(port));
-        }
-
-        public static int TryString2Ip(string str_val)
-        {
-            System.Net.IPAddress ip;
-
-            if (IPAddress.TryParse(str_val, out ip))
-            {
-                return System.BitConverter.ToInt32(ip.GetAddressBytes(), 0);
-            }
-
-            return 0;
-        }
-
-        public static int String2NumIp(string str_val)
-        {
-            System.Net.IPAddress ip = IPAddress.Parse(str_val);
-
-            return Ip2Num(ip);
-        }
-
-        public static void ParseIpPort(string val, out int num_ip, out short port)
-        {
-            Match m = Format.IpPortRegex.Match(val);
-
-            if (m.Success)
-            {
-                num_ip = String2NumIp(m.Groups["Ip"].Value);
-                
-                port = (short)UInt16.Parse(m.Groups["Port"].Value);
-
-                return;
-            }
-
-            num_ip = 0;
-
-            port = 0;
-        }
-
         public static string GetExceptionMessage(Exception x)
         {
             return GetExceptionMessage("The following error has occurred:\n{0}", x);
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # DataViewer
-Experimental WPF’s ICollectionView implementation based on Red-Black Tree
+Experimental WPF’s ICollectionView implementation based on Red-Black Tree.
 
+This is just an experimental C# project that demonstrates my coding style. It does not work stable so do not use it in real-world applications.
